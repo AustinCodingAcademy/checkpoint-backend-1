6c27c6efb1d879866cdeb21cbd0dd677759277d3
Matt Cameron <cameron.matt@gmail.com>
2018-05-16 12:22:23 -0500

    cleanup

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -20,7 +20,6 @@ module.exports.show = function (request, response) {
 
 module.exports.create = function (request, response) {
   const body = request.body;
-  console.log(body);
   const newTask = new Message({
     id: body.id || new Date().getMilliseconds(),
     name: body.name,
