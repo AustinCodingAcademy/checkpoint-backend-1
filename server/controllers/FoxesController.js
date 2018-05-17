00d15cb6524bd3a5a154c10ed065e02be0adefa6
Mike Mitchell <mikemitchell@Mikes-Upswing-MacBook-Pro.local>
2018-05-17 15:34:32 -0500

    changed fetch function slightly

--- server/controllers/FoxesController.js
+++ server/controllers/FoxesController.js
@@ -3,10 +3,7 @@ const fetch = require("node-fetch");
 
 
 module.exports.list = function list(request, response) {
-  fetch("https://randomfox.ca/floof/").then(res => res.json()).then(json => {
-    response.json(json.image);
-  });
-
+  fetch("https://randomfox.ca/floof/").then(res => res.json()).then(json => response.json(json.image));
 };
 
 
