6847159908539310b6d4ad1a330b8bc933b4759e
mimitch2 <mike@melodix.com>
2018-05-17 08:51:33 -0500

    fixed image size for the fox pics

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function DonutChart(props) {
+
+  const style = {
+    width: "100%"
+  };
+
   return (
     <div className="col-lg-4">
       <div className="panel panel-default">
@@ -9,7 +14,7 @@ function DonutChart(props) {
             <i className="fa fa-long-arrow-right fa-fw" /> Fox of the Day</h3>
         </div>
         <div className="panel-body">
-          <img src={props.foxes} />
+          <img src={props.foxes} style={style} />
         </div>
       </div>
     </div>);
