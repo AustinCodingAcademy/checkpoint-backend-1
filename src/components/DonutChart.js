c076cbe07ce5fbbaeac91d1f573056b0fb5160fe
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-05-21 18:52:09 -0500

    actaully fixed fox image

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -10,7 +10,7 @@ function DonutChart(props) {
                 <h3 className="panel-title"><i className="fa fa-long-arrow-right fa-fw"></i> Fox of the Day</h3>
             </div>
             <div className="panel-body">
-            <img style={{"height" : "100%"}} src={props.foxes} />
+            <img style={{"max-height" : "100%", "max-width" : "100%"}} src={props.foxes} />
             </div>
         </div>
     </div>);
