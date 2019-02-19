d77881eece7f32d68f3c5d1de06aee4ede03fa5d
Gabe <jflores353@gmail.com>
2019-02-19 16:59:20 -0600

    added FoxesRoutes.js but have not completed fetch call to url

--- server/Routes/TaskRoutes.js
+++ server/Routes/TaskRoutes.js
@@ -4,7 +4,7 @@ const { list, show, create, update } = require("../Controllers/TaskController");
 
 router.get("/tasks", list);
 router.get("/tasks/:id", show);
-router.post("/task", create);
-router.put("/task/:id", update);
+router.post("/tasks", create);
+router.put("/tasks/:id", update);
 
 module.exports = router;
