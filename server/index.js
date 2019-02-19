5b252f2dc0156adbbbd1404d5f077bf155d8ebae
Gabe <jflores353@gmail.com>
2019-02-19 15:33:31 -0600

    added folders in server for Routes, Models and Controllers

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use()
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://jflores353:$T1nkyi25!@ds343895.mlab.com:43895/checkpoint1', {useNewUrlParser: true});
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('Hello')
 })
 
-app.listen(3001);
+app.listen(3001, (err) => {
+    if (err) {
+        return console.log('Error', err);
+    }
+    console.log('Web server is now listening for messages on port 3001');
+});
