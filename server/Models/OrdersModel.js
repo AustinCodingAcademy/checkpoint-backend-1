b29a20e5843a602438b539a4cfc48c41f92aec9d
Gabe <jflores353@gmail.com>
2019-02-19 16:00:34 -0600

    created orderSchema for OrdersModel.js

---
+++ server/Models/OrdersModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+let orderSchema = new mongoose.Schema({
+    id: Number,
+    orderDate: String,
+    orderTime: String,
+    amount: String
+})
+
+module.exports = mongoose.model('Order', orderSchema);
