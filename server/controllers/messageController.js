b673f576b8f17e8523945c9445ca3dc751f140c5
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-08-02 04:28:38 -0500

    finished messages

---
+++ server/controllers/messageController.js
@@ -0,0 +1,23 @@
+let Message = require("../models/messageModel")
+
+exports.list =  function list(request, response) {
+   Message.find((e,v)=>{
+       return response.json(v);
+   });
+}
+exports.show = function show(request, response) {
+   Message.findById(request.params.id, (err,v)=>{
+       return response.json(v);
+   });
+}
+exports.create =  function create(request, response) {
+   let newMessage = new Message ({
+       id: request.body.id,
+       name: request.body.name,
+        date: request.body.date,
+        message: request.body.message
+   })
+   newMessage.save(()=>{
+       return response.json(newMessage);
+   });
+}
--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
 const app = express();
+// app.use(express.static("public"));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://kelly-nguyen-1:45683968kn@aca-practice-ss4lm.mongodb.net/checkpoint-backend-1?retryWrites=true&w=majority', {useNewUrlParser: true});
 
-//connect to database goes here:
-
+let foxRoute = require("./routes/foxRoute");
+let messageRoute  = require("./routes/messageRoute");
+let orderRoute = require("./routes/orderRoute");
+let taskRoute = require("./routes/taskRoute");
+let dataRoute = require("./routes/dataRoute")
 
 app.use(bodyParser.json());
-
+app.use(foxRoute);
+app.use(messageRoute);
+app.use(orderRoute);
+app.use(taskRoute);
+app.use(dataRoute);
 
 
 app.listen(3001, (err) => {
  if (err) {
    return console.log("Error", err);
  }
- console.log("Web server is now listening for messages", err);
+ console.log("Web server is now listening for messages on port",3001);
 });
