f1db9719651eafb19a165277607df843ea2f82c7
rmcbride2121 <rmcbride2121@gmail.com>
2019-08-04 16:29:33 -0500

    added folders, changes to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
-let express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
-app.use(express.static('./public'))//serve to local page
+let tasksRoutes = require("./Routes/tasksRoute")
+let ordersRoutes = require("./Routes/ordersRoute")
+let messagesRoutes = require("./Routes/messagesRoute")
+let foxRoute = require("./routes/foxRoute");
+let dataRoute = require("./routes/dataRoute")
 
-app.listen(3000)
+app.use(tasksRoutes);
+app.use(ordersRoutes);
+app.use(messagesRoutes);
+app.use(foxRoute);
+app.use(dataRoute);
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/checkpoint-backend', {useNewUrlParser: true});
+
+app.listen(3000, (err) => {
+ if (err) {
+   return console.log("Error", err);
+ }
+ console.log("Web server is now listening for messages on port",3000);
+});
