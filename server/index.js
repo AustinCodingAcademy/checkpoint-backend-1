43fb66fbf85cebe3627dca53b414fc262449a66b
Cris Smith <cris.smith.fa@gmail.com>
2019-08-09 19:05:12 -0500

    added node-fetch, finished files

--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ const mongoose = require('mongoose');
 const messageRoutes = require('../routes/messages');
 const orderRoutes = require('../routes/orders');
 const taskRoutes = require('../routes/tasks');
+const foxRoutes=require('../routes/foxes')
 
-mongoose.connect('mongodb+srv://GChivas:smitty5smitty5@cluster0-e2mbo.mongodb.net/checkpoint1db?retryWrites=true',{useNewUrlParser: true})
+
+mongoose.connect('mongodb://GChivas:smitty5smitty5@cluster0-shard-00-00-e2mbo.mongodb.net:27017,cluster0-shard-00-01-e2mbo.mongodb.net:27017,cluster0-shard-00-02-e2mbo.mongodb.net:27017/checkpoint1db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{useNewUrlParser: true})
 .then(()=>console.log("Connected to database..."))
 .catch(err=>console.log(err))
 
 let app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(messageRoutes, orderRoutes,taskRoutes);
+app.use(messageRoutes, orderRoutes,taskRoutes,foxRoutes);
+
 
 const PORT = process.env.PORT || 3000
 
