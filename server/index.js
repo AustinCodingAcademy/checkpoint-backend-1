90d518d0f67ec7f89c1602faa673c39c28ee1c61
dbobb220 <david.bobb.220@gmail.com>
2019-08-12 12:04:26 -0500

    mongoose, dotenv, and database creation

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config;
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@aca-practice-bx4sb.mongodb.net/test?retryWrites=true&w=majority`, {useNewUrlParser: true});
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send())
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send(`It's working`))
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
