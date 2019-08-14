c1db1658df7b616e2478c3af5bcc317ee7ccf370
daniela-idara <purpleelephant1987@gmail.com>
2019-08-13 21:35:24 -0500

    all network routes found

--- server/routes/newTasks.js
+++ server/routes/newTasks.js
@@ -4,7 +4,10 @@ const {list,show,create} = require("../controllers/tasks");
 
 // TASKS
 router.get('/tasks',list)
-router.get('/tasks/:id', show)
 router.post('/tasks', create)
+
+router.get('/taskItem',list)
+router.get('/taskItem/:id', show)
+router.post('/taskItem', create)
   
 module.exports = router;
